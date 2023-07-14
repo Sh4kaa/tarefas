69020d209a860dcac4cc45b98706f3fb61881829
Dario de souza <darioo_dario@hotmail.com>
2023-07-14 17:40:27 -0300

    main da home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import styles from '@/styles/home.module.css'
 import type { AppProps } from "next/app";
 import { Roboto } from "next/font/google";
 
@@ -6,7 +7,7 @@ const roboto = Roboto({ subsets: ["latin"], weight: "400" });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={roboto.className}>
+    <div className={`${roboto.className} ${styles.container}`}>
       <Component {...pageProps} />
     </div>
   );
