be5f7252b46c5e56b79d4bb7932c797cd71578dc
Dario de souza <darioo_dario@hotmail.com>
2023-07-19 17:24:47 -0300

    styles: Dashboard-section-2

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,8 +2,10 @@ import { GetServerSideProps } from "next";
 import { getSession } from "next-auth/react";
 import React from "react";
 import Head from "next/head";
-import styles from './styles.module.css'
+import styles from "./styles.module.css";
 import { Textarea } from "@/components/textarea";
+import { FiShare2 } from "react-icons/fi";
+import { FaTrash } from "react-icons/fa";
 
 export default function Dashboard() {
   return (
@@ -30,6 +32,26 @@ export default function Dashboard() {
             </form>
           </div>
         </section>
+
+        <section className={styles.taskContainer}>
+          <h1>Minhas tarefas</h1>
+
+          <article className={styles.task}>
+            <div className={styles.tagContainer}>
+              <label className={styles.tag}>PUBLICO</label>
+              <button className={styles.shareButton}>
+                <FiShare2 size={22} color="#3183ff" />
+              </button>
+            </div>
+
+            <div className={styles.taskContent}>
+              <p>Minha primeira tarefa de exemplo show demais!</p>
+              <button className={styles.trashButton}>
+                <FaTrash size={24} color="#ea3140" />
+              </button>
+            </div>
+          </article>
+        </section>
       </main>
     </div>
   );
@@ -38,7 +60,6 @@ export default function Dashboard() {
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   const session = await getSession({ req });
 
-  
   if (!session?.user) {
     return {
       redirect: {
