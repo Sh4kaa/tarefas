d2aafd3f37e0fc9871806fa0ea074fbb24f358ee
Dario Souza <darioo_dario@hotmail.com>
2023-08-20 13:17:29 -0300

    style: detalhes da tarefa

--- src/pages/task/[id].tsx
+++ src/pages/task/[id].tsx
@@ -3,16 +3,40 @@ import styles from "./styles.module.css";
 import { GetServerSideProps } from "next";
 import { doc, collection, query, where, getDoc } from "firebase/firestore";
 import { db } from "@/services/firebaseConnection";
+import { Textarea } from "@/components/textarea";
 
-export default function Task() {
+type TaskProps = {
+  dataTasks: {
+    public: boolean;
+    created: string;
+    user: string;
+    task: string;
+    taskID: string;
+  };
+};
+
+export default function Task({ dataTasks }: TaskProps) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Detalhes da tarefa</title>
+        <title>Tarefa - Detalhes da tarefa</title>
       </Head>
-      <div className={styles.main}>
+
+      <main className={styles.main}>
         <h1>Tarefa</h1>
-      </div>
+        <article className={styles.task}>
+          <p>{dataTasks.task}</p>
+        </article>
+      </main>
+
+      <section className={styles.commentsContainer}>
+        <h2>Deixar comentário</h2>
+
+        <form>
+          <Textarea placeholder="Digite seu comentário..." />
+          <button className={styles.button}>Enviar comentário</button>
+        </form>
+      </section>
     </div>
   );
 }
@@ -23,7 +47,7 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const docRef = doc(db, "Tasks", id);
   //puxando informações do banco por meio da referência
   const snapshot = await getDoc(docRef);
-// verificando se os dados são undefined, caso sejam, redirecione para página home
+  // verificando se os dados são undefined, caso sejam, redirecione para página home
   if (snapshot.data() === undefined) {
     return {
       redirect: {
@@ -43,8 +67,8 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   }
   // convertendo datas
   const miliseconds = snapshot.data()?.created.seconds * 1000;
-//montando o objeto para devolver ao front
-  const task = {
+  //montando o objeto para devolver ao front
+  const dataTasks = {
     public: snapshot.data()?.public,
     created: new Date(miliseconds).toLocaleDateString(),
     user: snapshot.data()?.user,
@@ -53,6 +77,6 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   };
 
   return {
-    props: { task },
+    props: { dataTasks },
   };
 };
